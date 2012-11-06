865e3faf06d8a483dde315d8617335c35d7e2e60
Shawn Tan <shawn@wtf.sg>
2012-11-06 17:12:29 +0800

    Draft.

--- final/report.d
+++ final/report.d
@@ -1,1 +1,1 @@
-report.dvi report.d : report.tex macros.tex introduction.tex related.tex method.tex metrics.tex evaluation.tex conclusion.tex  report.bib   
+report.dvi report.d : report.tex macros.tex introduction.tex related.tex method.tex metrics.tex evaluation.tex conclusion.tex appendix.tex  report.bib   
